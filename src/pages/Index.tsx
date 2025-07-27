feca20ed83cfabbdaaa9c3741697a3ae4ba69054
rafayet3994 <rafayet.cse@gmail.com>
2025-07-27 11:28:07 +0200

    add the latest drops slider in the homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 // Update this page (the content is just a fallback if you fail to update the page)
 
 import HeroSlider from '@/components/HeroSlider';
+import LatestDrops from '@/components/LatestDrops';
 import FeaturedProducts from '@/components/FeaturedProducts';
 import BrandSlider from '@/components/BrandSlider';
 import KollectionSlider from '@/components/KollectionSlider';
@@ -11,6 +12,7 @@ const Index = () => {
   return (
     <div className="min-h-screen">
       <HeroSlider />
+      <LatestDrops />
       <FeaturedProducts />
       <KollectionSlider />
       <FAQSection />
